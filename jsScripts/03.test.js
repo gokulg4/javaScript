98351fd2824b11c60ce88b4deedb52a3428c6db8
gokul.g@hotmail.com <gokul.g@hotmail.com>
2018-06-12 11:15:15 +0530

    Adding Project files

---
+++ jsScripts/03.test.js
@@ -0,0 +1,10 @@
+// console.log();
+
+
+//Failed to load JSLint library. Please install JSLint in your workspace folder
+// using 'npm install jslint' or globally using 'npm install -g jslint' and then press
+
+'use strict';
+
+var dataNotFetched = 'data not fetched';
+
